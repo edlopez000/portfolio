def6af922e098bf9a3f9751e044c3e9048905929
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-31 02:21:48 -0400

    Configured barebones Theme and ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { ThemeProvider, CssBaseline, Typography } from '@mui/material';
+import theme from './theme.js';
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography>Hello World!</Typography>
+    </ThemeProvider>
   );
 }
 
