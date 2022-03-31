60173d90a70f563336a176827f2014a860b76cf0
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-31 03:08:04 -0400

    Restructured main.jsx and reintroduced App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from '@emotion/react';
+import { Container, CssBaseline } from '@mui/material';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import About from './pages/About';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import theme from './theme';
+
+export default function App() {
+  return (
+    <Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Nav />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Container>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav';
-import About from './pages/About';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import theme from './theme.js';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Nav />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
