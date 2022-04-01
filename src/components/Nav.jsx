546d9512c8199a2c30ba5dc59c9f2f1b50ae096b
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-31 23:34:01 -0400

    react-router Links passed into MUI Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
-import { Link, Stack } from '@mui/material';
+import { Link as MUILink, Stack } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <Stack direction="row" spacing={2}>
-      <Link>Home</Link>
-      <Link>About Me</Link>
-      <Link>Projects</Link>
+    <Stack direction="row" spacing={2} justifyContent="center">
+      <MUILink component={Link} to="/">
+        Home
+      </MUILink>
+      <MUILink component={Link} to="about">
+        About Me
+      </MUILink>
+      <MUILink component={Link} to="projects">
+        Projects
+      </MUILink>
     </Stack>
   );
 }
