80e1ca80620b80b513c4149dab9d3abc3f552bf6
Edward Lopez-Ramos <lopez.edward@outlook.com>
2025-09-21 01:55:06 -0400

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import { Container, Typography } from '@mui/material';
-import React from 'react';
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
 export default function Home() {
   return (
     <Container
       sx={{
         flexGrow: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
       }}
     >
       <Typography variant="h4" align="center">
         Hi, I&apos;m <b>Edward</b>.
       </Typography>
       <Typography align="center">
-        I&apos;m a developer focusing on building platforms and products that
-        deliver.
+        I&apos;m a Production Support Engineer focusing on cloud infrastructure,
+        monitoring, and building reliable distributed systems.
       </Typography>
     </Container>
   );
